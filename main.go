c0a5831e3fe9562e09b6abae899720062e465ab9
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2022-06-12 16:42:08 +0900

    Refactor with named return value

--- main.go
+++ main.go
@@ -8,19 +8,29 @@ const frenchHelloPrefix = "Bonjour, "
 const japaneseHelloPrefix = "ご機嫌よう、"
 
 func Hello(name string, lang string) string {
-	prefix := englishHelloPrefix
 	if name == "" {
 		name = "World"
 	}
-	switch lang {
+
+	return greetingPrefix(lang) + name
+}
+
+//This is a named return value! A variable called prefix will be created in the function.
+//The initial value will depend on the type. For an int, 0; for a string, "".
+func greetingPrefix(language string) (prefix string) {
+	switch language {
 	case "Spanish":
 		prefix = spanishHelloPrefix
 	case "French":
 		prefix = frenchHelloPrefix
 	case "Japanese":
 		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	//We don't have to explicitly say `return prefix` since we've defined a named return value in the method declaration.
+	//return prefix
+	return
 }
 
 func main() {
