52479969411941a34eb9eef1c3b9588f2d1e0015
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2022-06-12 15:06:34 +0900

    Testing and general function syntax

---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	// t testing.TB:
+	// This is an interface that *testing.T and *testing.B both satisfy.
+	// This means that you can call this helper function from both a test, or a benchmark.
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// t.Helper() tells the test suite that this method is a helper.
+		// By doing this, when it fails the line number reported will be the function call rather than the helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
