02dce6da6f7939c66e91d2e6d006622daaeaab3e
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2022-06-12 16:31:59 +0900

    Refactor and add two more languages

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, lang string) string {
+	prefix := englishHelloPrefix
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	switch lang {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
