5d553d835e2472b042dc9845d50029906d6d8e57
jonathan.trudeau <trudeaujt@gmail.com>
2022-07-27 16:34:26 +0900

    Learned how to do property based tests to implement int to roman numeral conversion

--- src/properties/romannumerals.go
+++ src/properties/romannumerals.go
@@ -2,16 +2,61 @@ package properties
 
 import "strings"
 
-func ConvertToRoman(number int) string {
-	roman := strings.Builder{}
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
 
-	for i := number; i > 0; i-- {
-		if i == 4 {
-			roman.WriteString("IV")
-			break
+//When we add new values, we don't have to change the algorithm at all. Just add them to the list here.
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	result := strings.Builder{}
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
-		roman.WriteString("I")
+
 	}
 
-	return roman.String()
+	//This switch statement is describing some truths about roman numerals along with their behavior.
+	//We can, and should, refactor this by decoupling the data from the behavior.
+	/*for arabic > 0 {
+		switch {
+		case arabic > 9:
+			result.WriteString("X")
+			arabic -= 10
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+
+	}*/
+
+	return result.String()
 }
--- src/properties/romannumerals_test.go
+++ src/properties/romannumerals_test.go
@@ -1,22 +1,49 @@
 package properties
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic int
+		Roman  string
 	}{
-		{"1 gets converted to I", 1, "I"},
-		{"2 gets converted to II", 2, "II"},
-		{"3 gets converted to III", 3, "III"},
-		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{100, "C"},
+		{90, "XC"},
+		{400, "CD"},
+		{500, "D"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLXXXIV"},
+		{3999, "MMMCMXCIX"},
+		{2014, "MMXIV"},
+		{1006, "MVI"},
+		{798, "DCCXCVIII"},
 	}
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			want := test.Want
+			want := test.Roman
 			if got != want {
 				t.Errorf("got %q want %q", got, want)
 			}
