a2d5f489e3454fbfbab630826ec01db1bfd656a4
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2022-08-03 09:05:24 +0900

    Attempting implementation of blogpost file reader

---
+++ src/blogposts/cmd/main.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/trudeaujt/blogposts"
+	"log"
+	"os"
+)
+
+func main() {
+	posts, err := blogposts.NewPostsFromFS(os.DirFS("posts"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(posts)
+}
---
+++ src/blogposts/cmd/posts/one.md
@@ -0,0 +1,7 @@
+Title: This is the first post!
+Description: This is just a test post to make sure my blog backend is working.
+Tags: test, go
+---
+Here is the first line of my test.
+Here is the second line of my test.
+Goodbye.
